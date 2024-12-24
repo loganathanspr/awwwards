e7c37f6cc791c1506c9df212b5e8b69f3ae0f323
loganathanspr <loganathanspr@gmail.com>
2024-12-24 17:28:05 +0100

    Navbar with music

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -144,7 +144,7 @@ const Hero = () => {
               id="watch-trailer"
               title="Watch Trailer"
               leftIcon={<TiLocationArrow />}
-              containerClasses="!bg-yellow-300 flex-center gap-1"
+              containerClass="!bg-yellow-300 flex-center gap-1"
             />
           </div>
         </div>
